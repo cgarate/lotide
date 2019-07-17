d043fb174a8d69ae2b89df0762add3d8697b2684
Carlos Garate <carlos.garate@rangle.io>
2019-07-17 19:47:35 -0400

    creates assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = (actual, expected) =>
+  actual === expected
+    ? console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`)
+    : console.log(`❌ Assertion Failed: [${actual}] === [${expected}]`);
+// Test Code
+assertEqual("Carlos", "Karla");
+assertEqual(1, 1);
+assertEqual("Carlos", "Carlos");
+assertEqual(10, 11);
